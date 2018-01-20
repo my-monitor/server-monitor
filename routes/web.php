2de304b672bf3e625a62e6578a9ca8500a739e32
Ahmed Ashraf <ahmed29329@gmail.com>
2018-01-20 19:44:58 +0200

    Foce auth

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/servers', 'ServersController@index')->name('servers.home');
-Route::get('/servers/create', 'ServersController@create')->name('servers.create');
-Route::post('/servers', 'ServersController@store')->name('servers.store');
-Route::get('/servers/{id}/show', 'ServersController@show')->name('servers.show');
-Route::get('/servers/{id}/edit', 'ServersController@edit')->name('servers.edit');
-Route::put('/servers/{id}/update', 'ServersController@update')->name('servers.update');
+Route::group(['middleware' => 'auth'],function (){
+    Route::get('/', 'ServersController@index')->name('servers.home');
+    Route::get('/home', 'ServersController@index')->name('servers.home');
+    Route::get('/servers', 'ServersController@index')->name('servers.home');
+    Route::get('/servers/create', 'ServersController@create')->name('servers.create');
+    Route::post('/servers', 'ServersController@store')->name('servers.store');
+    Route::get('/servers/{id}/show', 'ServersController@show')->name('servers.show');
+    Route::get('/servers/{id}/edit', 'ServersController@edit')->name('servers.edit');
+    Route::put('/servers/{id}/update', 'ServersController@update')->name('servers.update');
+});
